2e52f744e9877621ce440ecb17bc500b16586d19
William Henke <william.henke@computingforall.org>
2021-05-08 16:45:50 -0700

    flex has been implemented and pokeCard creation has been cleaned up

--- script.js
+++ script.js
@@ -27,24 +27,31 @@ fetch('https://pokeapi.co/api/v2/pokemon/')
             getData(ele.url)
             .then(data => {
                 let section = document.createElement('div');
-                section.className = "pokeStats";
+                section.className = "pokeChart";
 
+                let picture1 = document.createElement('div');
+                picture1.className = "pokeStats";
                 let paragraph1 = document.createElement('p');
                 paragraph1.innerHTML = data.name.charAt(0).toUpperCase() + data.name.slice(1);
-                section.append(paragraph1);
-                let paragraph2 = document.createElement('p');
-                paragraph2.innerHTML = "Shiny " + data.name.charAt(0).toUpperCase() + data.name.slice(1);
-                section.append(paragraph2);
-
+                picture1.append(paragraph1);
                 let image1 = document.createElement('img');
-                let image2 = document.createElement('img');
                 image1.src = data.sprites.front_default;
                 image1.className = "image";
-                section.append(image1);
+                picture1.append(image1);
+
+
+                let picture2 = document.createElement('div');
+                picture2.className = "pokeStats";
+                let paragraph2 = document.createElement('p');
+                paragraph2.innerHTML = "Shiny " + data.name.charAt(0).toUpperCase() + data.name.slice(1);
+                picture2.append(paragraph2);
+                let image2 = document.createElement('img');
                 image2.src = data.sprites.front_shiny;
-                section.append(image2);
                 image2.className = "image";
+                picture2.append(image2);
                 
+                section.appendChild(picture1);
+                section.appendChild(picture2);
                 pokedex.appendChild(section);
             })
         })
